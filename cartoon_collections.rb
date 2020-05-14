8106da3147105b1a6b325db5646724b00a091866
Valentina Panic <valentina.panic87@gmail.com>
2020-05-14 01:02:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 end
 
 def find_the_cheese(cheddar_cheese)# code an argument here
-cheddar_cheese.find {|items| puts items == "cheddar" }
+cheddar_cheese.find {|items| puts items == cheese_types }
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
